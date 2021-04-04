275a55be514253840e025856a4be274ad1ee69a1
cambrown101 <cameron-brown@live.com>
2021-04-04 15:34:50 -0700

    Learn about python variables

--- PythonBasics/code.py
+++ PythonBasics/code.py
@@ -1,9 +1,29 @@
 # creating variables
-x = 15
-price = 9.99
 
-discount = 0.2
+# numbers
+# x = 15
+# price = 9.99
 
-result = price * (1 - discount)
+# discount = 0.2
 
-print(result)
+# result = price * (1 - discount)
+
+# print(result)
+
+# Strings
+name = 'Cam'
+
+# Concatinates Strings
+# print(name * 2)
+
+# Changing variable names
+a = 25
+b = a
+print(a)
+print(b)
+
+# Changes the value
+b = 17
+
+print(a)
+print(b)
