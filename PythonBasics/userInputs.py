5c8a0b6e970dc046982475faa9e2f627678bb0cd
cambrown101 <cameron-brown@live.com>
2021-04-05 15:53:05 -0700

    round decimal number

--- PythonBasics/userInputs.py
+++ PythonBasics/userInputs.py
@@ -3,4 +3,5 @@ size_input = input("How big is your house (in square feet): ")
 square_feet = int(size_input)
 
 square_meters = square_feet / 10.8
-print(f"{size_input} square feet is equal to {square_meters} square meters")
+
+print(f"{size_input} square feet is equal to {square_meters:.2f} square meters")
