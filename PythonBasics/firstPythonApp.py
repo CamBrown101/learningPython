e212a0dc2e35af4db47aeca81bcaf1c6bede4c7f
cambrown101 <cameron-brown@live.com>
2021-04-05 15:59:42 -0700

    Create a app that takes a users age and breaks it down into seconds

---
+++ PythonBasics/firstPythonApp.py
@@ -0,0 +1,8 @@
+years = int(input("How old are you?"))
+months = years * 12
+weeks = months * 4
+days = weeks * 7
+hours = days * 24
+minutes = days * 60
+seconds = minutes * 60
+print(f"You are {months} months old, {weeks} weeks old, {days} days old, {hours} hours old, {minutes} minutes old, {seconds} seconds old!")
