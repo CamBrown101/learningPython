83d34b28f0d0db4685d3cb48ce1178e35520b883
cambrown101 <cameron-brown@live.com>
2021-04-05 16:59:15 -0700

    Refresher on In keyword

---
+++ PythonBasics/in_keyword.py
@@ -0,0 +1,8 @@
+# friends = ["Rolf", "Bob"]
+# print("Rolf" in friends)
+
+movies_watched = {"a", "b", "c"}
+
+user_movie = input("Enter something you've watched latley: ")
+
+print(user_movie in movies_watched)
