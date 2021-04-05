5d78ccc1b6839c23e77ae80aca414251627c1361
cambrown101 <cameron-brown@live.com>
2021-04-05 16:15:34 -0700

    Learn list operations

---
+++ PythonBasics/advanced_set_operations.py
@@ -0,0 +1,14 @@
+# friends = {"Bob", "Sam"}
+# abroad = {"Cam"}
+
+# local_friends = friends.difference(abroad)
+# print(local_friends)
+
+# total_friends = friends.union(abroad)
+# print(total_friends)
+
+art = {"Bob", "Sam", "Rolf", "Charlie"}
+science = {"Bob", "Jen", "Adam", "Sam"}
+
+both = art.intersection(science)
+print(both)
