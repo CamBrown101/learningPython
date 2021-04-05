f2aa49beadd764d2ea1f5117f50fa95326085226
cambrown101 <cameron-brown@live.com>
2021-04-05 16:37:55 -0700

    Brief boolean lesson

---
+++ PythonBasics/booleans.py
@@ -0,0 +1,9 @@
+print(5 == 5)
+print(5 > 5)
+print(10 != 10)  # does not equal
+
+friends = {"Bob", "Sam"}
+abroad = {"Bob", "Sam"}
+
+print(friends == abroad)
+print(friends is abroad) # don't use is
