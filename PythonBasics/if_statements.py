6fa35b64f5b2e70045660ec2c89e380010bd344a
cambrown101 <cameron-brown@live.com>
2021-04-05 16:56:21 -0700

    Refresher on conditionals

---
+++ PythonBasics/if_statements.py
@@ -0,0 +1,10 @@
+day_of_week = input("day of week?").lower()
+
+if day_of_week == "monday":
+    print("Have a great start to your week!")
+elif day_of_week == "Tuesday":
+    print("tuesday")
+else:
+    print("Not Monday")
+
+print("This always runs.")
