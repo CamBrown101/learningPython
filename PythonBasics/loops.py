5ca014a9460751987216d3b1934ad33cc79764e2
cambrown101 <cameron-brown@live.com>
2021-04-05 20:54:24 -0700

    Refresher on loops

---
+++ PythonBasics/loops.py
@@ -0,0 +1,29 @@
+# number = 7
+
+# while True:
+#     user_input = input("Would you like to play? (Y/n) ")
+
+#     if user_input == "n":
+#         break
+
+#     user_number = int(input("Guess a number: "))
+#     if user_number == number:
+#         print("You guessed correctly!")
+#     elif abs(number - user_number) == 1:
+#         print("You were off by 1")
+#     else:
+#         print("Wrong!")
+
+
+# friends = ["Cam", "Sam", "Bob"]
+# for friend in range(4):  # will run 4 times
+#     print(f"{friend} is my friend.")
+
+grades = [35, 67, 98, 100, 100]
+total = sum(grades)
+amount = len(grades)
+
+# for grade in grades:
+#     total += grade
+
+# print(total / amount)
