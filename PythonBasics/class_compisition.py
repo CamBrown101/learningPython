a0bfcde6012526b54274443002368d1e1f0aaa6c
cambrown101 <cameron-brown@live.com>
2021-04-12 21:27:55 -0700

    Understanding compisition

---
+++ PythonBasics/class_compisition.py
@@ -0,0 +1,25 @@
+class Bookshelf:
+    def __init__(self, *books):
+        self.books = books
+
+    def __str__(self):
+        return f"Bookshelf with {len(self.books)} books"
+
+
+class Book:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"Book {self.name}"
+
+    def __repr__(self):
+        return f"Book {self.name}"
+
+
+book = Book("Harry Potter")
+book2 = Book("Harry Potter 2")
+shelf = Bookshelf(book, book2)
+
+print(shelf)
+print(shelf.books)
